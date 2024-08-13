24f40ddc1b743af383d43faae0cd152d32ced55b
Kieran Klaassen <kieranklaassen@gmail.com>
2024-08-13 13:38:58 -0500

    Refactor foreign key constraints in schema.rb

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -68,9 +68,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_13_174509) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "leva_dataset_records", "datasets"
-  add_foreign_key "leva_evaluation_results", "dataset_records"
-  add_foreign_key "leva_evaluation_results", "experiments"
-  add_foreign_key "leva_experiments", "datasets"
-  add_foreign_key "leva_experiments", "prompts"
+  add_foreign_key "leva_dataset_records", "leva_datasets", column: "dataset_id"
+  add_foreign_key "leva_evaluation_results", "leva_dataset_records", column: "dataset_record_id"
+  add_foreign_key "leva_evaluation_results", "leva_experiments", column: "experiment_id"
+  add_foreign_key "leva_experiments", "leva_datasets", column: "dataset_id"
+  add_foreign_key "leva_experiments", "leva_prompts", column: "prompt_id"
 end
