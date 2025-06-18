37e77ee3973170ec9c4d1ccd7c5aad040ab6ab84
Kieran Klaassen <kieranklaassen@gmail.com>
2025-06-18 16:10:37 -0700

    Fix linting issues: add missing newlines and remove trailing whitespace

--- test/controllers/leva/assets_test.rb
+++ test/controllers/leva/assets_test.rb
@@ -5,7 +5,7 @@ module Leva
     test "workbench loads without CDN dependencies" do
       get leva.workbench_index_path
       assert_response :success
-      
+
       # Should not contain CDN references
       assert_no_match(/cdn\.tailwindcss\.com/, response.body)
       assert_no_match(/cdn\.jsdelivr\.net/, response.body)
